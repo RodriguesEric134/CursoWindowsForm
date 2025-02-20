c4e9f28ded4562081d8d73e3098e74e0f8c740fe
RodriguesEric134 <rm550249@fiap.com.br>
2025-02-20 16:56:42 -0300

    finalizado curso 1, iniciando curso 2

---
+++ CursoWindowsForms/Forms Curso 1/Frm_ValidaCPF2.cs
@@ -0,0 +1,75 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF2 : Form
+    {
+        public Frm_ValidaCPF2()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_ValidaCPF2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string validaConteudo;
+            validaConteudo = Msk_CPF.Text;
+            validaConteudo = validaConteudo.Replace(".", "").Replace("-", "");
+            validaConteudo = validaConteudo.Trim();
+            if(validaConteudo == "")
+            {
+                MessageBox.Show("NÃO TEM NADA INSERIDO AQUI", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if(validaConteudo.Length != 11)
+                {
+                    MessageBox.Show("O CPF DEVE TER 11 DÍGITOS", "Você é um ET por acaso?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        if (MessageBox.Show("Você tem absoluta certeza?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            if (MessageBox.Show("Quer mesmo?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                            {
+                                if (MessageBox.Show("Última chance!!", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                                {
+                                    bool validaCPF = false;
+                                    validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
+                                    if (validaCPF == true)
+                                    {
+                                        MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }               
+            }                        
+        }
+    }
+}
---
+++ CursoWindowsForms/Frm_Principal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal : Form
+    {
+        public Frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_DemontracaoKey_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey DK = new Frm_DemonstracaoKey();
+            DK.ShowDialog();
+        }
+
+        private void Btn_HelloWorld_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld HW = new Frm_HelloWorld();
+            HW.ShowDialog();
+        }
+
+        private void Btn_Mascara_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara MCR = new Frm_Mascara();
+            MCR.ShowDialog();
+        }
+
+        private void Btn_ValidaCPF_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF CPF = new Frm_ValidaCPF();
+            CPF.ShowDialog();
+        }
+
+        private void Btn_ValidaCPF2_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 CPF2 = new Frm_ValidaCPF2();
+            CPF2.ShowDialog();
+        }
+
+        private void Btn_ValidaSenha_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha SNH = new Frm_ValidaSenha();
+            SNH.ShowDialog();
+        }
+    }
+}
