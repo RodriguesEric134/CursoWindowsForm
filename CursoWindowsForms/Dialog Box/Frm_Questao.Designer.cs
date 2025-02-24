d8fd584be253d3434ed9a6638bd5512542f312a0
RodriguesEric134 <rm550249@fiap.com.br>
2025-02-24 17:15:04 -0300

    por hoje é isso, seguirei depois com o resto da aula

--- CursoWindowsForms/Dialog Box/Frm_Questao.Designer.cs
+++ CursoWindowsForms/Dialog Box/Frm_Questao.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Lbl_Questao = new System.Windows.Forms.Label();
+            this.Btn_OK = new System.Windows.Forms.Button();
+            this.Btn_Cancel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl_Questao
+            // 
+            this.Lbl_Questao.AutoSize = true;
+            this.Lbl_Questao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Questao.Location = new System.Drawing.Point(18, 9);
+            this.Lbl_Questao.Name = "Lbl_Questao";
+            this.Lbl_Questao.Size = new System.Drawing.Size(244, 20);
+            this.Lbl_Questao.TabIndex = 0;
+            this.Lbl_Questao.Text = "Você deseja validar a senha?";
+            // 
+            // Btn_OK
+            // 
+            this.Btn_OK.Location = new System.Drawing.Point(147, 60);
+            this.Btn_OK.Name = "Btn_OK";
+            this.Btn_OK.Size = new System.Drawing.Size(115, 23);
+            this.Btn_OK.TabIndex = 1;
+            this.Btn_OK.Text = "SIM QUERO";
+            this.Btn_OK.UseVisualStyleBackColor = true;
+            this.Btn_OK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Btn_Cancel
+            // 
+            this.Btn_Cancel.Location = new System.Drawing.Point(147, 89);
+            this.Btn_Cancel.Name = "Btn_Cancel";
+            this.Btn_Cancel.Size = new System.Drawing.Size(115, 23);
+            this.Btn_Cancel.TabIndex = 2;
+            this.Btn_Cancel.Text = "NÃO QUERO";
+            this.Btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 97);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Frm_Questao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Frm_Questao";
+            this.ClientSize = new System.Drawing.Size(289, 151);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Btn_Cancel);
+            this.Controls.Add(this.Btn_OK);
+            this.Controls.Add(this.Lbl_Questao);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_Questao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Questão";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Lbl_Questao;
+        private System.Windows.Forms.Button Btn_OK;
+        private System.Windows.Forms.Button Btn_Cancel;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
